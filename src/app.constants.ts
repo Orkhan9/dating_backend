891b9a4fbad1f6cf9e10775ed8fec6387039017c
orkhan999 <orkhan@finverity.com>
2024-06-09 17:30:41 +0400

    feat: typeorm configuration

--- src/app.constants.ts
+++ src/app.constants.ts
@@ -1,1 +1,6 @@
 export const PORT = 'PORT';
+export const DB_HOST = 'DB_HOST';
+export const DB_PORT = 'DB_PORT';
+export const DB_NAME = 'DB_NAME';
+export const DB_USERNAME = 'DB_USERNAME';
+export const DB_PASSWORD = 'DB_PASSWORD';
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {
+  DB_HOST,
+  DB_NAME,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_USERNAME,
+} from './app.constants';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>(DB_HOST, 'localhost'),
+        port: configService.get<number>(DB_PORT, 5432),
+        username: configService.get<string>(DB_USERNAME),
+        password: configService.get<string>(DB_PASSWORD),
+        database: configService.get<string>(DB_NAME),
+        autoLoadEntities: true,
+        synchronize: false,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
