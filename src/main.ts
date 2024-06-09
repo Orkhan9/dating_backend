2becc9198ef6441b65a36fcc4f510818d0ab702b
orkhan999 <orkhan@finverity.com>
2024-06-09 16:50:00 +0400

    feat: dotenv config postgres packages

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { PORT } from './app.constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = app.get(ConfigService).get<number>(PORT) || 3001;
+  await app.listen(port);
 }
-bootstrap();
+
+bootstrap()
+  .then(() =>
+    console.log('app started successfully on port ' + process.env.PORT),
+  )
+  .catch((error) => console.log('app could not start ', error));
